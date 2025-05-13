626dcfc73d1e97ea705ec16b7e9e271a211cfd52
Fernanda Alves <fernandaalves.tds2.senai@gmail.com>
2025-05-13 08:21:00 -0300

    refactor: adjust project

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,10 +2,6 @@ import express from 'express';
 
 // Importar todas as rotas
 import authRouter from './auth.routes.js';
-import animeRouter from './animeRoutes.js';
-import personagemRouter from './personagemRoutes.js';
-import collectionRouter from './collectionRoutes.js';
-import cardRouter from './cardRoutes.js';
 
 import authMiddleware from '../middleware/authMiddleware.js';
 
@@ -13,12 +9,9 @@ const router = express.Router();
 
 // Rotas públicas
 router.use('auth', authRouter);
-router.use('/collection', collectionRouter);
-router.use('/card', cardRouter)
 
 // Rotas protegidas
 router.use(authMiddleware);
-router.use('/anime', animeRouter);
-router.use('/personagem', personagemRouter);
+
 
 export default router;
